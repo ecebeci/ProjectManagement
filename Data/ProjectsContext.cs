e880a0627b55efd4eef8be4f3d17c4847b22dbb8
Emre <0emrecebeci0@gmail.com>
2021-11-18 15:39:02 +0000

    Create Board and Project Model, Create Data Context Class,  Edit Startup.cs, Add ER CRUD Pages to View

---
+++ Data/ProjectsContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Data
+{
+    public class ProjectsContext : DbContext
+    {
+       public ProjectsContext(DbContextOptions<ProjectsContext> options)
+                : base(options)
+            {
+            }
+
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Board> Boards { get; set; }
+        // TODO
+        //public DbSet<List> Lists { get; set; }
+        //public DbSet<Work> Works { get; set; }
+
+    }
+}
