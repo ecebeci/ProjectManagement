15dcea2535a429f47fb26213d7ec9fc8e0ec2aca
Yusuf Eren Cam <yusuf_yusuf_cam@hotmail.com>
2021-11-18 20:54:03 +0000

    list model adding

---
+++ Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Models
+{
+    public class List
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Title { get; set; }
+        //public List<Work> Works { get; set; } = new List<Work>();
+    }
+
+}
