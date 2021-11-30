56f61dd17f0c7e8598fb85cb6cd741ef586225f9
Emre <0emrecebeci0@gmail.com>
2021-11-30 21:20:34 +0000

    Add User-Project Relationship

---
+++ ViewModels/MembersProject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.ViewModels
+{
+    public class MembersProject
+    {
+    }
+}
