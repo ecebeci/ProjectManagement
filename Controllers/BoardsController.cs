80af6bb3dfb8f90c4e0cf106d9c06730146f210b
Emre <0emrecebeci0@gmail.com>
2021-12-07 14:58:02 +0000

    Add Boards Controller, Edit  Board/Index View

---
+++ Controllers/BoardsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Data;
+using ProjectManagement.Models;
+
+namespace ProjectManagement.Controllers
+{
+    public class BoardsController : Controller
+    {
+        private readonly ProjectsContext _context;
+
+        public BoardsController(ProjectsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Boards/5
+        [Authorize(Roles = "member")]
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null) // if there is no id on route-id
+            {
+                return RedirectToAction("Index", "Projects"); // returns to projects page
+            }
+
+            var project = await _context.Project
+              .Include(b => b.Boards) // Relation!
+              .FirstOrDefaultAsync(m => m.ProjectId == id);
+
+            // if the project not found
+            if (project == null)
+            {
+                return RedirectToAction("Index", "Projects"); // returns to projects page
+            }
+
+            var boards = project.Boards;
+
+            if (boards.Count == 0) // If the project has any boards
+            {
+                return View("Create");
+            }
+
+            return View(boards); // boards
+        }
+
+        // GET: Boards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board
+                .Include(b => b.Project)
+                .FirstOrDefaultAsync(m => m.BoardId == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // GET: Boards/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjectId"] = new SelectList(_context.Project, "ProjectId", "Name");
+            return View();
+        }
+
+        // POST: Boards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BoardId,ProjectId,Title,BoardDescription,CreatedDate,UpdatedDate")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(board);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Project, "ProjectId", "Name", board.ProjectId);
+            return View(board);
+        }
+
+        // GET: Boards/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board.FindAsync(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Project, "ProjectId", "Name", board.ProjectId);
+            return View(board);
+        }
+
+        // POST: Boards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BoardId,ProjectId,Title,BoardDescription,CreatedDate,UpdatedDate")] Board board)
+        {
+            if (id != board.BoardId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(board);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoardExists(board.BoardId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Project, "ProjectId", "Name", board.ProjectId);
+            return View(board);
+        }
+
+        // GET: Boards/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board
+                .Include(b => b.Project)
+                .FirstOrDefaultAsync(m => m.BoardId == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // POST: Boards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var board = await _context.Board.FindAsync(id);
+            _context.Board.Remove(board);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoardExists(int id)
+        {
+            return _context.Board.Any(e => e.BoardId == id);
+        }
+    }
+}
