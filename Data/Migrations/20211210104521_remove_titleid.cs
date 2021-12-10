c5a035a1863fe1385ba09578f16d572879b1c9a2
Emre <0emrecebeci0@gmail.com>
2021-12-10 10:46:51 +0000

    Add titleid remove migration, Edit Projects/Edit View

---
+++ Data/Migrations/20211210104521_remove_titleid.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectManagement.Migrations.Projects
+{
+    public partial class remove_titleid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleId",
+                table: "Member");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TitleId",
+                table: "Member",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
