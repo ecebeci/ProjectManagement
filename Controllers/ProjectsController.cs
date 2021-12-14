df558a3ad1f042af1ad62104fc3b99c235afcb6b
Emre <0emrecebeci0@gmail.com>
2021-12-14 14:33:09 +0000

    Add adding and removing members to the project

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -241,8 +241,7 @@ namespace ProjectManagement.Controllers
             var ProjectMembers = await _context.ProjectMember
                 .Where(x => x.Project == project)
                 .Include(u => u.Member)
-                .Include(u => u.Project.Manager)
-                .ToListAsync();
+                .Include(u => u.Project.Manager).ToListAsync();
 
             ProjectProjectMembers ProjectProjectMembers = new ProjectProjectMembers // Setting View Model
             {
@@ -290,8 +289,72 @@ namespace ProjectManagement.Controllers
             return View(project);
         }
 
-        // GET: Projects/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        [HttpPost]
+        [HttpPost, ActionName("AddMemberProject")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "member")]
+        public async Task<IActionResult> AddMemberProject([Bind("Username")] Member member, [Bind("ProjectId")] Project project)
+        {
+            if (member.Username == null)
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Message = "You have entered invalid username. Please check the name!";
+                return View("Failed");
+            }
+
+            member = await _context.Member
+                         .Where(x=> x.Username == member.Username)
+                         .FirstOrDefaultAsync();
+
+            if (member == null)
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Message = "The username which you entered is not registered! Please check the name again!";
+                return View("Failed");
+            }
+
+            var ProjectMemberCheck = await _context.ProjectMember
+                                        .Include(x => x.Member)
+                                        .Where(x => x.ProjectId == project.ProjectId)
+                                        .Where(x => x.Member.Username == member.Username)
+                                        .ToListAsync();
+
+            if (ProjectMemberCheck.Any())
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Message = "You entered a person who already exists in the project. Please check the name!";
+                return View("Failed");
+            }
+
+        
+            try
+                {
+                    //  project.UpdatedDate = DateTime.Now;
+                    _context.ProjectMember.Add(new ProjectMember
+                    {
+                        ProjectId = project.ProjectId,
+                        MemberId = member.MemberId
+                    });
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemberExits(project.ProjectId, member.MemberId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+             }
+            
+
+            return RedirectToAction(nameof(Index));
+        }
+
+            // GET: Projects/Delete/5
+            public async Task<IActionResult> Delete(int? id)
         {
 
             if (id == null)
@@ -332,7 +395,41 @@ namespace ProjectManagement.Controllers
             return View(ProjectProjectMembers);
         }
 
-        // POST: Projects/Delete/5
+        // POST: Projects/DeleteMember/@pm
+        [HttpPost, ActionName("DeleteMember")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "member")]
+        public async Task<IActionResult> DeleteMemberOnProjectConfirmed([Bind("ProjectId, MemberId")]  ProjectMember pm)
+        {
+            // adding Project and Member objects 
+            var ProjectMember = await _context.ProjectMember 
+               .Where(x => x.ProjectId == pm.ProjectId)
+               .Where(x => x.MemberId == pm.MemberId)
+               .Include(u => u.Member)
+               .Include(u => u.Project.Manager)
+               .FirstOrDefaultAsync();
+
+            if (ProjectMember == null)
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Message = "Project couldn't find.";
+                return View("Failed");
+            }
+
+            if(ProjectMember.Project.ManagerId == ProjectMember.Member.MemberId)
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Message = "You can't discard yourself on Project. To delete project go to Delete Project";
+                return View("Failed");
+            }
+
+            _context.ProjectMember.Remove(ProjectMember);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+            // POST: Projects/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "member")]
@@ -369,5 +466,10 @@ namespace ProjectManagement.Controllers
         {
             return _context.Project.Any(e => e.ProjectId == id);
         }
-    }
+
+        private bool MemberExits(int ProjectId,int MemberId)
+            {
+                return _context.ProjectMember.Any(e => e.ProjectId == ProjectId && e.MemberId == MemberId);
+        }
+        }
 }
